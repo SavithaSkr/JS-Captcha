30d55ab9d39e6e5aed9fcfa2b1449d620a3ebf32
SavithaSkr <savitha848410@gmail.com>
2024-07-03 11:09:44 -0400

    updated timer

--- assests/quiz.js
+++ assests/quiz.js
@@ -1,4 +1,3 @@
-//store the data of userinformation in a variable
 const userData = {};
 
 let allQuestions = [];
@@ -6,70 +5,110 @@ let selectedQuestion = [];
 let currentQuestionidx = 0;
 let userSelectedAnswer = [];
 let mark = 0;
+const ttlQuestionCount = 10;
+let timer;
+const totalQuizTime = 120; // Total time in seconds for the entire quiz
+let remainingTime = totalQuizTime;
 
-const ttlQuestionCount = 2;
-//selecting 10 random question function
+// Select 10 random questions function
 const selectRandomQuesiton = () => {
-  //storing shuffledquestions in a variable
   let shuffledQuestions = allQuestions.sort(() => 0.5 - Math.random());
-  /* console.log(
-    "shuffled random quesion",
-    (selectedQuestion = shuffledQuestions.slice(0, ttlQuestionCount))
-  ); */
-  //this line returns first 10 questions
   selectedQuestion = shuffledQuestions.slice(0, ttlQuestionCount);
+};
+
+const startQuizTimer = () => {
+  const timerElement = document.getElementById("timer");
+  timer = setInterval(() => {
+    if (remainingTime <= 0) {
+      clearInterval(timer);
+      loadingPage("result.html");
+    } else {
+      remainingTime--;
+      const minutes = Math.floor(remainingTime / 60);
+      const seconds = remainingTime % 60;
+      timerElement.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
+        seconds < 10 ? "0" : ""
+      }${seconds}`;
+    }
+  }, 1000);
 };
 
 const showQuestions = () => {
   const currentQuestion = selectedQuestion[currentQuestionidx];
-  console.log(currentQuestion, selectedQuestion);
   const question_container = document.getElementById("question_container");
 
   question_container.innerHTML = `
-  <h4>Question ${currentQuestionidx + 1} : ${currentQuestion.question}</h4> 
-  ${currentQuestion.answers.map((ans, idx) => {
-    return `
+    <h4>Question ${currentQuestionidx + 1} : ${currentQuestion.question}</h4> 
+    ${currentQuestion.answers
+      .map((ans, idx) => {
+        return `
+      <div class="quiz_ques">
+          <label>
+              <input type="radio" name="answer" value="${idx}">${ans}</input>
+          <br>
+          </label>
+      </div>`;
+      })
+      .join("")}
     <div>
-        <label>
-            <input type="radio" name="answer" value="${idx}">${ans}</input>
-        <br>
-        </label>
-    </div>`;
-  })}
+      <button id="previous" ${
+        currentQuestionidx === 0 ? "disabled" : ""
+      }>Previous</button>
+      <button id="next">${
+        currentQuestionidx === ttlQuestionCount - 1 ? "Finish" : "Next"
+      }</button>
+    </div>
   `;
+
+  document.getElementById("next").addEventListener("click", () => {
+    let selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (
+      selectedAnswer &&
+      selectedAnswer.value == selectedQuestion[currentQuestionidx].correct
+    ) {
+      mark++;
+    }
+    userSelectedAnswer[currentQuestionidx] = selectedAnswer
+      ? selectedAnswer.value
+      : null;
+    currentQuestionidx++;
+    if (currentQuestionidx < ttlQuestionCount) {
+      showQuestions();
+    } else {
+      loadingPage("result.html");
+    }
+  });
+
+  document.getElementById("previous").addEventListener("click", () => {
+    currentQuestionidx--;
+    showQuestions();
+  });
 };
+
 const showAnswer = () => {
   const answerContainer = document.getElementById("answerContainer");
   answerContainer.innerHTML = selectedQuestion
     .map(
       (question, qno) =>
         `
-  <div>
-    <p>Question ${qno + 1}:${question.question}</p>
-
-   ${question.answers
-     .map((ans, ansidx) => {
-       var color = "black";
-
-       if (question.correct == ansidx) {
-         console.log(question.correct, ansidx);
-         color = "green";
-         console.log(color);
-       } /* else if (userSelectedAnswer[qno] == ansidx);
-       {
-         color = "red";
-       } */
-       console.log(color);
-       return `
-    <p style="color:${color}">${ans}</p>
-     `;
-     })
-     .join(" ")}
-    </div>`
+      <div>
+        <p>Question ${qno + 1}:${question.question}</p>
+        ${question.answers
+          .map((ans, ansidx) => {
+            var color = "black";
+            if (question.correct == ansidx) {
+              color = "green";
+            } else if (userSelectedAnswer[qno] == ansidx) {
+              color = "red";
+            }
+            return `<p style="color:${color}">${ans}</p>`;
+          })
+          .join(" ")}
+      </div>`
     )
     .join(" ");
 };
-//initialize the page
+
 const initialiseApp = (path) => {
   if (path === "landing.html") {
     document.getElementById("userForm").addEventListener("submit", (event) => {
@@ -79,67 +118,27 @@ const initialiseApp = (path) => {
 
       userData.name = userName;
       userData.mail = userMail;
-
-      /* console.log("i m here"); */
       loadingPage("instruction.html");
     });
   } else if (path === "instruction.html") {
     document.getElementById("startQuiz").addEventListener("click", () => {
-      //loading quiz app page
       loadingPage("quiz_question.html");
     });
   } else if (path === "quiz_question.html") {
+    startQuizTimer(); // Start the timer when the quiz page loads
     showQuestions();
-
-    //next page
-    document.getElementById("next").addEventListener("click", () => {
-      let selectedAnswer = document.querySelector(
-        'input[name = "answer"]:checked'
-      );
-      if (
-        selectedAnswer.value == selectedQuestion[currentQuestionidx].correct
-      ) {
-        mark++;
-        console.log("mark is", mark);
-      } else {
-        console.log("no mark mark", mark);
-      }
-      userSelectedAnswer[currentQuestionidx] = selectedAnswer.value;
-      currentQuestionidx++;
-
-      if (currentQuestionidx < ttlQuestionCount) {
-        showQuestions();
-      } else {
-        console.log("i came here");
-        loadingPage("result.html");
-      }
-    });
-
-    //previous page
-    /* document.getElementById("previous").addEventListener("click", () => {
-    currentQuestionidx--;
-    if (currentQuestionidx == ttlQuestionCount) {
-      showQuestions();
-    } else {
-      loadingPage("result.html");
-    }
-  }); */
   } else if (path === "result.html") {
-    console.log("mark is", mark);
-
     document.getElementById(
       "mark"
-    ).textContent = `your score is ${mark} / ${ttlQuestionCount}`;
-    console.log("result here");
+    ).textContent = `Your score is ${mark} / ${ttlQuestionCount}`;
     showAnswer();
   }
 };
-//getting the page
+
 const loadingPage = (path) => {
   fetch(`./assests/pages/${path}`)
     .then((data) => data.text())
     .then((html) => {
-      console.log("loading page html");
       document.getElementById("quizapp").innerHTML = html;
       initialiseApp(path);
     })
@@ -148,13 +147,10 @@ const loadingPage = (path) => {
     });
 };
 
-//loading the dom envirment
 document.addEventListener("DOMContentLoaded", () => {
   loadingPage("landing.html");
-  //loading the questions when the page is loaded
   fetch("questions.json").then((data) =>
     data.json().then((questions) => {
-      console.log("questions", questions);
       allQuestions = questions;
       selectRandomQuesiton();
     })
